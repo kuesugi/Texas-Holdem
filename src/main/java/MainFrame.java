2cfbdeb11cad12e280c0176fa47a2a302173d3fd
Joey Ma <jackfield1611@gmail.com>
2018-06-04 21:50:17 -0400

    add basic UI

--- src/main/java/MainFrame.java
+++ src/main/java/MainFrame.java
@@ -1,21 +1,84 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.io.*;
 import javax.swing.*;
 import javax.imageio.*;
-import java.io.*;
 
 public class MainFrame extends JFrame {
+	// the frame structure
+	// TODO: AI names
+	private Image background;
+	private JButton northAI1 = new JButton("1");
+	private JButton northAI2 = new JButton("2");
+	private JButton northAI3 = new JButton("3");
+    private JButton player = new JButton("Player");
+    private JButton pot = new JButton("Pot");
+    private JButton eastAI1 = new JButton("E");
+    private JButton eastAI2 = new JButton("E");
+    private JButton westAI1 = new JButton("W");
+    private JButton westAI2 = new JButton("W");
+    // the player name
+    private String userName = new String();
+    // money in the pot
+    private double moneyInPot = 0;
+    // new game button
+    private JButton[] _newGame = new JButton[1];
+    // exit button
+    private JButton[] _exit = new JButton[1];
+    // dealer
+    private JButton[] _dealer = new JButton[1];
+		
 	public MainFrame() {
 		super("Texas Holdem");
+		initFrame();
+        // set the size of the frame
+        setSize(1190, 1250);
+        // click X to exit
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Must be the last line of this constructor
+        setVisible(true);
+	}
+	
+	private void initFrame(){
+		// load the background image
+		getContentPane().setLayout(null);
+		JLabel label = new JLabel("");
+		try {
+			background = ImageIO.read(getClass().getResource("/background.jpg"));
+		}
+		catch (IOException ioex){
+		    System.exit(1);
+		}
+		label.setIcon(new ImageIcon(background));
+		label.setBounds(0, 0, 1190, 1250);	// TODO: not full windowed background
+		//getContentPane().add(label);
 		
-		// name of the frame
-	    JFrame _frame = new JFrame("Texas Holdem");
-	    // new game button
-	    JButton[] _newGame = new JButton[1];
-	    // exit button
-	    JButton[] _exit = new JButton[1];
-	    // dealer
-	    JButton[] _dealer = new JButton[1];
+		// JPanel mainPanel = new JPanel();
+		setExtendedState(JFrame.MAXIMIZED_BOTH);
+		// NORTH
+        northAI1.setBounds(95,30,310,130);
+        add(northAI1);
+        northAI2.setBounds(481,30,310,130);
+        add(northAI2);
+        northAI3.setBounds(866,30,310,130);
+        add(northAI3);
+		// WEST
+        westAI1.setBounds(10,180,310,130);
+        add(westAI1);
+        westAI2.setBounds(10,330,310,130);
+        add(westAI2);
+        // CENTER
+        pot.setBounds(326,180,630,320);
+        add(pot);
+        // EAST
+        eastAI1.setBounds(962,180,310,130);
+        add(eastAI1);
+        eastAI2.setBounds(962,330,310,130);
+        add(eastAI2);
+        // SOUTH
+        player.setBounds(165,506,957,160);
+        add(player);
+		
 	    /*// Card images
 	    Image 2C; Image 2D; Image 2H; Image 2S; Image AC; Image AD; Image AH; Image AS;
 	    Image 3C; Image 3D; Image 3H; Image 3S; Image JC; Image JD; Image JH; Image JS;
@@ -25,11 +88,5 @@ public class MainFrame extends JFrame {
 	    Image 7C; Image 7D; Image 7H; Image 7S; Image 8C; Image 8D; Image 8H; Image 8S;
 	    Image 9C; Image 9D; Image 9H; Image 9S; Image 10C; Image 10D; Image 10H; Image 10S;*/
 
-        // set the size of the frame
-        _frame.setSize(1000,1000);
-        // click X to exit
-        _frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // Must be the last line of this constructor
-        _frame.setVisible(true);
 	}
 }
