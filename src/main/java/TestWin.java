86e1386c1fa5034fe78ef7b84ba23d4e84d2fd81
Jay Berdimuratov <jab395@pitt.edu>
2018-06-20 03:23:54 -0400

    Improvements To Game Panel

--- src/main/java/TestWin.java
+++ src/main/java/TestWin.java
@@ -2,7 +2,7 @@
 public class TestWin {
 
 	public static void main(String[] args) {
-		
+		int score = 0;
 		Card card1 = new Card(2,1);
 		Card card2 = new Card(7,1);
 		Card card3 = new Card(13,1);
@@ -10,7 +10,7 @@ public class TestWin {
 		Card card5 = new Card(1,1);
 		Card card6 = new Card(4,3);
 		Card card7 = new Card(3,10);
-		Hand hand1 = new Hand(card1,card2);
+		Hand hand1 = new Hand(card1,card2,card3);
 		
 		hand1.addCard(card3);
 		hand1.addCard(card4);
@@ -18,7 +18,6 @@ public class TestWin {
 		hand1.addCard(card6);
 		hand1.addCard(card7);
 		
-		int score = hand1.checkScore();
 		System.out.println(score);
 
 	}
