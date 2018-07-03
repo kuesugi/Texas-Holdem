fa7995260c1ef530e223b5c073498b2babac491a
Jay Berdimuratov <terexberd@yahoo.com>
2018-07-03 18:15:25 -0400

    Update resultFrame.java

--- src/main/java/resultFrame.java
+++ src/main/java/resultFrame.java
@@ -1,14 +1,21 @@
 import java.awt.EventQueue;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import java.awt.Color;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 import java.awt.BorderLayout;
 import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+
 import java.awt.Font;
 import javax.swing.JButton;
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
@@ -20,26 +27,45 @@ public class resultFrame extends JFrame {
 
 	/**
 	 * Create the application.
+	 * @param mainFrame 
+	 * @param players 
+	 * @param user 
 	 */
-	public resultFrame(String result) {
-		initialize(result);
+	public resultFrame(String result, Player user, ArrayList<Player> players, MainFrame mainFrame) {
+		initialize(result, user, players, mainFrame);
 	}
 
 	/**
 	 * Initialize the contents of the frame.
+	 * @param mainFrame 
+	 * @param players 
+	 * @param user 
 	 */
-	private void initialize(String result) {
+	private void initialize(String result, Player user, ArrayList<Player> players, MainFrame mainFrame) {
 		frame = new JFrame();
+        JPanel btnPnl = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        JPanel mainPanel = new JPanel(new BorderLayout());
 		frame.getContentPane().setBackground(new Color(0, 128, 0));
 		
 		JLabel label = new JLabel(result);
-		label.setFont(new Font("Gill Sans MT Ext Condensed Bold", Font.BOLD, 31));
+		label.setFont(new Font("Gill Sans MT Ext Condensed Bold", Font.BOLD, 15));
 		label.setForeground(new Color(255, 255, 255));
 		label.setHorizontalAlignment(SwingConstants.CENTER);
 		
 		JButton quitButton = new JButton("Quit");
-		quitButton.setFont(new Font("Gill Sans MT Condensed", Font.BOLD, 12));
+		JButton restart = new JButton("Restart");
+		JButton nextHandButton = new JButton("Next Hand");
+
+        btnPnl.add(restart);
+        btnPnl.add(quitButton);
+        btnPnl.add(nextHandButton);
 		
+        btnPnl.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		mainPanel.setBackground(new Color(0, 128, 0));
+
+        mainPanel.add(label, BorderLayout.CENTER);
+        mainPanel.add(btnPnl, BorderLayout.SOUTH);
+
 		quitButton.setBackground(new Color(50, 205, 50));
 		quitButton.addActionListener(new ActionListener() {
 	        public void actionPerformed(ActionEvent e) {
@@ -47,45 +73,28 @@ public class resultFrame extends JFrame {
 	        }
 	    });
 		
-		JButton nextHandButton = new JButton("Next Hand");
-		nextHandButton.setFont(new Font("Gill Sans MT Condensed", Font.BOLD, 12));
-		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
-		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(label, GroupLayout.DEFAULT_SIZE, 964, Short.MAX_VALUE)
-					.addContainerGap())
-				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
-					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addContainerGap()
-							.addComponent(nextHandButton, GroupLayout.PREFERRED_SIZE, 192, GroupLayout.PREFERRED_SIZE))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(396)
-							.addComponent(quitButton, GroupLayout.DEFAULT_SIZE, 150, Short.MAX_VALUE)))
-					.addGap(396))
-		);
-		groupLayout.setVerticalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
-					.addGap(65)
-					.addComponent(label, GroupLayout.PREFERRED_SIZE, 58, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED, 34, Short.MAX_VALUE)
-					.addComponent(nextHandButton, GroupLayout.PREFERRED_SIZE, 31, GroupLayout.PREFERRED_SIZE)
-					.addGap(18)
-					.addComponent(quitButton, GroupLayout.PREFERRED_SIZE, 31, GroupLayout.PREFERRED_SIZE)
-					.addGap(24))
-		);
-		frame.getContentPane().setLayout(groupLayout);
-		frame.setBounds(100, 100, 1000, 300);
+		restart.setBackground(new Color(50, 205, 50));
+		restart.addActionListener(new ActionListener() {
+	        public void actionPerformed(ActionEvent e) {
+				mainFrame.setVisible(false);
+				mainFrame.dispose();
+				frame.setVisible(false);
+	            MainMenu game = new MainMenu();
+	            game.setVisible(true);	
+	        }
+	    });
+		
+        frame.add(mainPanel);
+		frame.setBounds(100, 100, 1100, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 		
-		
+
 		nextHandButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+				mainFrame.setVisible(false);
+				mainFrame.dispose();
+				new MainFrame(user, players, frame);
 				
 			}
 		});
