e0bad82ced00b16e75a6710902f96a9045cd7c6a
Jay Berdimuratov <terexberd@yahoo.com>
2018-07-03 13:59:27 -0400

    Restart Game Button

--- src/main/java/resultFrame.java
+++ src/main/java/resultFrame.java
@@ -1,13 +1,18 @@
 import java.awt.EventQueue;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import java.awt.Color;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 import java.awt.BorderLayout;
 import javax.swing.SwingConstants;
 import java.awt.Font;
+
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 
 public class resultFrame extends JFrame implements ActionListener {
@@ -26,23 +31,45 @@ public class resultFrame extends JFrame implements ActionListener {
 	 */
 	private void initialize(String result) {
 		frame = new JFrame();
-		frame.getContentPane().setBackground(new Color(0, 128, 0));
-		
+        JPanel btnPnl = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        JPanel mainPanel = new JPanel(new BorderLayout());
+
 		JLabel label = new JLabel(result);
 		label.setFont(new Font("Luminari", Font.PLAIN, 18));
 		label.setForeground(new Color(255, 255, 255));
 		label.setHorizontalAlignment(SwingConstants.CENTER);
-		frame.getContentPane().add(label, BorderLayout.CENTER);
-		
+
+		JButton restart = new JButton("Restart");
 		JButton close = new JButton("Close");
-		
+
+        btnPnl.add(restart);
+        btnPnl.add(close);
+        
+        btnPnl.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		mainPanel.setBackground(new Color(0, 128, 0));
+
+        mainPanel.add(label, BorderLayout.CENTER);
+        mainPanel.add(btnPnl, BorderLayout.SOUTH);
+
+        
 		close.setBackground(new Color(50, 205, 50));
 		close.addActionListener(new ActionListener() {
 	        public void actionPerformed(ActionEvent e) {
 	        	System.exit(0);	
 	        }
 	    });
-		frame.getContentPane().add(close, BorderLayout.SOUTH);
+		
+		restart.setBackground(new Color(50, 205, 50));
+		restart.addActionListener(new ActionListener() {
+	        public void actionPerformed(ActionEvent e) {
+	        	dispose();
+	        	frame.setVisible(false);
+	            MainMenu game = new MainMenu();
+	            game.setVisible(true);	
+	        }
+	    });
+	
+        frame.add(mainPanel);
 		frame.setBounds(100, 100, 1000, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
